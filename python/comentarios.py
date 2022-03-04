216f46d4c680bf4a26d54737a15dce8fc7239a8d
PET - ADS <94725313+petadsifsp@users.noreply.github.com>
2022-03-04 13:39:36 -0300

    Update comentarios.py

--- python/comentarios.py
+++ python/comentarios.py
@@ -4,3 +4,13 @@
 Comentário de
 múltiplas linhas
 """
+
+""" ----- REDES SOCIAIS -----
+Email: pet.ads.ifsp@gmail.com
+Facebook: https://www.facebook.com/PETADSIFSP
+GitHub: https://github.com/petadsifsp
+Instagram: https://www.instagram.com/petadsifsp/
+Site: http://pet_ads.paginas.scl.ifsp.edu.br/
+TikTok: https://vm.tiktok.com/ZMLNJ7pTb/
+Twitter: https://twitter.com/pet_ads_ifsp
+YouTube: https://www.youtube.com/channel/UC92DC8TQL6KfxVS2mKemXiw """
