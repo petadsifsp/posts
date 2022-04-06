3bc0d1fe5347185cac51958f283ae5149d2554e9
PET - ADS <94725313+petadsifsp@users.noreply.github.com>
2022-04-05 21:04:28 -0300

    Update sintaxe.py

--- python/sintaxe.py
+++ python/sintaxe.py
@@ -17,6 +17,4 @@ Facebook: https://www.facebook.com/PETADSIFSP
 GitHub: https://github.com/petadsifsp
 Instagram: https://www.instagram.com/petadsifsp/
 Site: http://pet_ads.paginas.scl.ifsp.edu.br/
-TikTok: https://vm.tiktok.com/ZMLNJ7pTb/
-Twitter: https://twitter.com/pet_ads_ifsp
 YouTube: https://www.youtube.com/channel/UC92DC8TQL6KfxVS2mKemXiw """
