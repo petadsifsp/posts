bb3d2de4c59020c44cadbe4a82c15964ab65135e
PET - ADS <94725313+petadsifsp@users.noreply.github.com>
2022-04-06 23:59:25 -0300

    Update conversao.py

--- python/conversao.py
+++ python/conversao.py
@@ -1,3 +1,10 @@
 inteiro = 2010
 string = str(inteiro)
 
+""" ----- REDES SOCIAIS -----
+Email: pet.ads.ifsp@gmail.com
+Facebook: https://www.facebook.com/PETADSIFSP
+GitHub: https://github.com/petadsifsp
+Instagram: https://www.instagram.com/petadsifsp/
+Site: http://pet_ads.paginas.scl.ifsp.edu.br/
+YouTube: https://www.youtube.com/channel/UC92DC8TQL6KfxVS2mKemXiw """
