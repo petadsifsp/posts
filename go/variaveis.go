9672b84ca80f6a85ac5effa8e2145030bf9e6f8c
PET - ADS <94725313+petadsifsp@users.noreply.github.com>
2022-04-06 23:56:34 -0300

    variables ok

--- go/variaveis.go
+++ go/variaveis.go
@@ -10,3 +10,11 @@ func main() {
 	pet = "ADS"
 	ano = 2022
 }
+
+/* ----- REDES SOCIAIS -----
+Email: pet.ads.ifsp@gmail.com
+Facebook: https://www.facebook.com/PETADSIFSP
+GitHub: https://github.com/petadsifsp
+Instagram: https://www.instagram.com/petadsifsp/
+Site: http://pet_ads.paginas.scl.ifsp.edu.br/
+YouTube: https://www.youtube.com/channel/UC92DC8TQL6KfxVS2mKemXiw */
