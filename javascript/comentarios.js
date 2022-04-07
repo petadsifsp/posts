aa51675661ed6238d854a213d5b3e082b5129241
PET - ADS <94725313+petadsifsp@users.noreply.github.com>
2022-04-06 23:58:44 -0300

    comments ok

--- javascript/comentarios.js
+++ javascript/comentarios.js
@@ -2,3 +2,11 @@
 
 /* Comentário
 explicativo */
+
+/* ----- REDES SOCIAIS -----
+Email: pet.ads.ifsp@gmail.com
+Facebook: https://www.facebook.com/PETADSIFSP
+GitHub: https://github.com/petadsifsp
+Instagram: https://www.instagram.com/petadsifsp/
+Site: http://pet_ads.paginas.scl.ifsp.edu.br/
+YouTube: https://www.youtube.com/channel/UC92DC8TQL6KfxVS2mKemXiw */
